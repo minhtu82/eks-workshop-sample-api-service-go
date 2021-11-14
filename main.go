49a868f3068bc0ca2e0a4233d3bf6f0212e5faca
minhtu82 <19685964+minhtu82@users.noreply.github.com>
2021-11-14 13:25:01 +0700

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Welcome to CI/CD Pipeline lab-Tudoan-VJ"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
